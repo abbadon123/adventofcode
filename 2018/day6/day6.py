d8e80c82c6d151ecc0dfa99a65e532a8c532ba01
Mateusz Kanarek <Mateusz.Kanarek@ig.com>
2018-12-17 11:40:23 +0100

    day6

--- 2018/day6/day6.py
+++ 2018/day6/day6.py
@@ -15,9 +15,8 @@ def closest_coordinate(coordinates):
             closest.append(c)
         if d < min_distance:
             min_distance = d
-            closest = []
-            closest.append(c)
-    return closest
+            closest = [c]
+    return closest, min_distance
 
 
 if __name__ == '__main__':
@@ -39,12 +38,22 @@ if __name__ == '__main__':
                     distances[(x, y)] = distance_from_point_to_x_y
         closest_coordinates = []
         for coordinate_distances in distances.values():
-            if len(closest_coordinate(coordinate_distances)) == 1:
-                closest_coordinates.append(closest_coordinate(coordinate_distances)[0])
+            closest_coordinates_list, _ = closest_coordinate(coordinate_distances)
+            if len(closest_coordinates_list) == 1:
+                closest_coordinates.append(closest_coordinates_list[0])
+        # finite_coordinates is not working ... but one of the answers is ok, the answer is 4284
+        finite_coordinates = [coordinate for coordinate in closest_coordinates if min_x <
+                              coordinate[0] < max_x and min_y < coordinate[1] < max_y]
+        pprint.pprint(
+            sorted(Counter(finite_coordinates).items(), key=lambda coord_to_count: coord_to_count[1], reverse=False))
+        # ((198, 248), 4284), this is ok
+        # ((53, 309), 4403),
+        # ((131, 322), 5099)]
 
-        sorted_closest_coordinates = sorted(Counter(closest_coordinates).items(), key=lambda x: x[1], reverse=True)
-        # finite_coordinates is not working, but second answer is ok
-        finite_coordinates = [coordinate for coordinate in sorted_closest_coordinates if coordinate[0][0] > min_x and coordinate[0][0] < max_x and coordinate[0][1] > min_y and coordinate[0][1] < max_y]
-        pprint.pprint(finite_coordinates)
-        # [((131, 322), 4529),
-        #  ((198, 248), 4284),
+        # part 2
+        count = 0
+        for x in range(min_x, max_x + 1):
+            for y in range(min_y, max_y + 1):
+                if sum([distance((x, y), coordinate) for coordinate in coordinates]) < 10000:
+                    count = count + 1
+        print(count)
