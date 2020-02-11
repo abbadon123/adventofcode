0b0028bbc2d75c2bbb881058651080b5fb0b727c
Mateusz Kanarek <Mateusz.Kanarek@ig.com>
2020-02-11 15:10:57 +0100

    2019 day2 part2

--- 2019/src/adventofcode_2019/day2/day_2.clj
+++ 2019/src/adventofcode_2019/day2/day_2.clj
@@ -3,49 +3,53 @@
 
 
 (defn computer [memory]
-  {:memory   memory
-   :position 0})
+  {:memory              memory
+   :instruction-pointer 0})
 
-(defn position-value [computer n]
+(defn address-value [computer n]
   (let [memory (:memory computer)
-        position (:position computer)
-        n-position (+ n position)]
-    (memory n-position)))
+        instruction-pointer (:instruction-pointer computer)
+        address-n (+ n instruction-pointer)]
+    (memory address-n)))
 
 (defn operation [computer]
-  (let [opcode (position-value computer 0)
-        operation (case opcode
+  (let [instruction (address-value computer 0)
+        operation (case instruction
                     1 +
                     2 *)]
     operation))
 
-(defn arg-n [computer n]
-  (let [address (position-value computer n)
+(defn parameter-n [computer n]
+  (let [address (address-value computer n)
         memory (:memory computer)
         value (memory address)]
     value))
-(defn arg1 [computer]
-  (arg-n computer 1))
-(defn arg2 [computer]
-  (arg-n computer 2))
-(defn result-address [computer]
-  (position-value computer 3))
 
+(defn parameter-1 [computer]
+  (parameter-n computer 1))
+
+(defn parameter-2 [computer]
+  (parameter-n computer 2))
+
+(defn result-address [computer]
+  (address-value computer 3))
 
 (defn execute [computer]
-  (let [arg1 (arg1 computer)
-        arg2 (arg2 computer)
+  (let [parameter-1 (parameter-1 computer)
+        parameter-2 (parameter-2 computer)
         operation (operation computer)
-        result (operation arg1 arg2)
+        result (operation parameter-1 parameter-2)
         result-adr (result-address computer)
         update-memory (fn [memory] (assoc memory result-adr result))]
     (->
       computer
       (update-in [:memory] update-memory)
-      (update-in [:position] + 4))))
+      (update-in [:instruction-pointer] + 4))))
 
 (defn halt? [computer]
-  (= 99 (position-value computer 0)))
+  (let [instruction (address-value computer 0)]
+    (not (or (= instruction 1)
+             (= instruction 2)))))
 
 (defn run-program [memory]
   (loop [computer (computer memory)]
@@ -63,21 +67,20 @@
 
 
 (run-tests)
-(use 'clojure.tools.trace)
-(trace-ns adventofcode-2019.day2.day-2)
-(untrace-ns adventofcode-2019.day2.day-2)
-;;
 
-(->
-  [1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,9,19,1,19,5,23,2,6,23,27,1,6,27,31,2,31,9,35,1,35,6,39,1,10,39,43,2,9,43,47,1,5,47,51,2,51,6,55,1,5,55,59,2,13,59,63,1,63,5,67,2,67,13,71,1,71,9,75,1,75,6,79,2,79,6,83,1,83,5,87,2,87,9,91,2,9,91,95,1,5,95,99,2,99,13,103,1,103,5,107,1,2,107,111,1,111,5,0,99,2,14,0,0]
-  (assoc 1 12)
-  (assoc 2 2)
-  run-program
-  first)
+(defn run [noun verb]
+  (->
+    [1, 0, 0, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 1, 9, 19, 1, 19, 5, 23, 2, 6, 23, 27, 1, 6, 27, 31, 2, 31, 9, 35, 1, 35, 6, 39, 1, 10, 39, 43, 2, 9, 43, 47, 1, 5, 47, 51, 2, 51, 6, 55, 1, 5, 55, 59, 2, 13, 59, 63, 1, 63, 5, 67, 2, 67, 13, 71, 1, 71, 9, 75, 1, 75, 6, 79, 2, 79, 6, 83, 1, 83, 5, 87, 2, 87, 9, 91, 2, 9, 91, 95, 1, 5, 95, 99, 2, 99, 13, 103, 1, 103, 5, 107, 1, 2, 107, 111, 1, 111, 5, 0, 99, 2, 14, 0, 0]
+    (assoc 1 noun)
+    (assoc 2 verb)
+    run-program
+    first))
 
-;; intcode computer
-;; integers -> memory
-;; position
-;; opcode 1 2 99
-;; move to next one
-;; segment 4 ints
+(run 12 2)
+
+(let [match? (fn [[x y]] (= (run x y) 19690720))
+      pairs (for [noun (range 0 100) verb (range 0 100)] [noun verb])
+      matches (filter match? pairs)
+      match (first matches)
+      [noun verb] match]
+  (+ (* 100 noun) verb))
